cb3416a7057b2e196f867b9fc0cf714801a7afd0
Mary Luna <mary.luna.salcedo@gmail.com>
2025-02-28 14:30:22 -0500

    final touches to code

--- Solucion_Desafio/Desafio.py
+++ Solucion_Desafio/Desafio.py
@@ -1,16 +1,14 @@
 notas = []
 while len(notas) < 5:
-    nota = int(input("Porfavor, ingrese sus notas individualmente: "))
+    nota = int(input("Porfavor, ingrese sus notas individualmente:"))
     notas.append(nota)
-
+    
 suma = sum(notas)
 promedio = suma/5
 
 if promedio >= 60:
     print(f"Su promedio es {promedio}, usted ha aprobado la materia")
-elif promedio == 59 and promedio > 40:
+elif promedio < 60 and promedio >= 40:
     print(f"Su promedio es {promedio}, usted actualmente se encuentra en recuperación")
-elif promedio <= 40:
-    print(f"Su promedio es {promedio}, usted ha reprobado")
 else:
-    print("notas no válidas")
+    print(f"Su promedio es {promedio}, usted ha reprobado")
